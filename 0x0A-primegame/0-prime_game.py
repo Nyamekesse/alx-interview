2c19698fc8bee699517bab2c803a5edb78528470
“Nyamekesse” <snyamekesse@stu.ucc.edu.gh>
2023-09-10 01:52:24 +0000

    fixed error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
-""" Module for solving prime game question """
+"""
+name of the player that won the most round
+"""
+
+
+def get_primes(nums):
+    """
+    the winner can be determine
+    """
+    n = max(nums)
+    if n < 1:
+        return None
+    prime_cnt = [0] * len(nums)
+    prime = [True for _ in range(n + 1)]
+    p = 2
+    prime[0] = prime[1] = False
+    while (p * p <= n):
+        if prime[p]:
+            for j in range(p * 2, n + 1, p):
+                prime[j] = False
+        p += 1
+    for i in range(len(nums)):
+        cnt = 0
+        for j in range(nums[i] + 1):
+            if (prime[j]):
+                cnt += 1
+        prime_cnt[i] = cnt
+    return prime_cnt
+
 
 def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
+    """
+    play x round of the game
+    """
+    if x <= 0 or nums is None or min(nums) < 0:
         return None
-    max_num = max(nums)
-
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
+    prime_cnt = get_primes(nums)
+    if prime_cnt is None:
         return None
-    if player1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
+    loses = 0
+    for i in prime_cnt:
+        if i % 2 == 0:
+            loses += 1
+    if x < 2 * loses:
+        return "Ben"
+    if x == 2 * loses:
+        return None
+    return "Maria"
